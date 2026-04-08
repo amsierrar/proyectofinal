a1aab7075e5ecc4d1a75c2ce53171f4acdedee88
amsierrar <tutorbogota@outlook.es>
2026-04-07 22:44:28 -0500

    cambio db.js para conectar a vercel

--- db.js
+++ db.js
@@ -1,12 +1,35 @@
-const mongoose = require('mongoose');
-require ('dotenv').config()
+const mongoose = require("mongoose");
 
-const dbconnect = () => {
-    mongoose.set('strictQuery', true);
+const MONGO_URL = process.env.MONGO_URL;
 
-    mongoose.connect(process.env.MONGO_URL)
-        .then(() => console.log("Conexión exitosa a tecquimdb"))
-        .catch((err) => console.log("Error:", err.message));
+if (!MONGO_URL) {
+  throw new Error("⚠️ Falta la variable MONGO_URL");
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbconnect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    mongoose.set("strictQuery", true);
+
+    cached.promise = mongoose.connect(MONGO_URL, {
+      bufferCommands: false,
+    }).then((mongoose) => {
+      console.log("✅ Conectado a MongoDB");
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 module.exports = dbconnect;
